836695337b483139c38ce883df4c0664a4a30906
jungissei <issei_matsuhashi@yahoo.co.jp>
2020-04-01 13:01:38 +0900

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2017_05_09_232014) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "posts", force: :cascade do |t|
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.boolean "published"
